64f45d3a6c43fd1228fd3fb2936cbb42669ebf58
Nate <8bitsodagamer@gmail.com>
2025-03-18 13:10:04 +0400

    fullready

--- superVector/main.cpp
+++ superVector/main.cpp
@@ -9,20 +9,17 @@ struct Point
 
 int main()
 {
-    SuperVector<int> vector{ 1,2,3 };
-    SuperVector<int> vector1{ 1,1,3 };
-    //SuperVector<Point> vector {{1,1}, {2,2}, {3,3} };
-    //SuperVector<Point> vector1 {{1,1}, {2,2}, {3,3} };
-    //SuperVector<Point> vector1 {{11,-1}, {-2,-2}, {-3,33} };
-    //vector += vector1;
-    //vector.Pop();
-    //vector.Remove(0);
-    //vector.Insert(0, {2,3});
-    std::cout << (vector == vector1);
-    //for (size_t i = 0; i < vector.Size(); i++)
-    //{
-    //    std::cout << vector[i].x << vector[i].y << "\n";
-    //}
+    SuperVector<Point> vector {{1,1}, {2,2}, {3,3} };
+    SuperVector<Point> vector1 {{1,1}, {2,2}, {3,3} };
+    SuperVector<Point> vector1 {{11,-1}, {-2,-2}, {-3,33} };
+    vector += vector1;
+    vector.Pop();
+    vector.Remove(0);
+    vector.Insert(0, {2,3});
+    for (size_t i = 0; i < vector.Size(); i++)
+    {
+        std::cout << vector[i].x << vector[i].y << "\n";
+    }
 
 
     return 0;
